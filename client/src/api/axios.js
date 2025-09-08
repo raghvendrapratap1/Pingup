958f312490cbfb5979c36a6a9e31375bbea14365
Raghvendra pratap Singh <raghvendrapratapsingh505@gmail.com>
2025-09-08 11:02:01 +0530

    3rd commit

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const baseURL = import.meta.env.VITE_BASEURL || "https://pingup-back.vercel.app/";
+const baseURL = import.meta.env.VITE_BASEURL || "https://pingup-back.vercel.app";
 
 const api = axios.create({
   baseURL,
